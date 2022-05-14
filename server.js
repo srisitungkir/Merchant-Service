ca95631d9a3552080bdc0212ab883255919f274a
srisitungkir <sri.situngkir39@gmail.com>
2022-05-14 21:49:54 +0700

    add code routers in server.js

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+//Aplikasi JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//memanggil router
+let routers = require('./routers');
+routers(app);
+
 
  app.listen(3000, () => {
      console.log('Server started on port 3000');
