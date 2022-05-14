181df265251d8e6c1e2143c28af2dc26041b795e
srisitungkir <sri.situngkir39@gmail.com>
2022-05-14 21:23:26 +0700

    add file connection.js

---
+++ connection.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+//Membuat koneksi database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jaya@3388',
+    database: 'merchant_service'
+});
+
+db.connect((err) => {
+    if(err) throw err;
+    console.log('Mysql has connected');
+});
+
+module.exports = db; 
