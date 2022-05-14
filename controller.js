86db454a6e5a35d8f2a94f9cd9d45d79034b20fb
srisitungkir <sri.situngkir39@gmail.com>
2022-05-14 23:32:57 +0700

    add route to show all merchant information

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ let connection = require('./connection');
 exports.index = function(req, res) {
     response.ok("Rest API working...", res)    
 };
+
+//menampilkan data merchant information
+exports.allmerchantinformation = function(req, res) {
+    connection.query('SELECT * FROM merchant_information', function(error, rows, fileds) {
+        if(error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
+
